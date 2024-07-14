b9d6574f48ad0b9ef80d5b7a1a7265db29b62a9c
vineshkannaa <136898323+vineshkannaa@users.noreply.github.com>
2024-07-14 22:01:47 +0530

    Time: 4 ms (72.82%) | Memory: 13.5 MB (17.51%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         
@@ -11,4 +11,42 @@ public:
         }
         return false;
     }
+
+        unordered_map<int, int> hash;
+        
+        for(int i=0; i<arr.size(); i++){
+            hash[arr[i]*2]=arr[i];
+        }
+        for(int i=0; i<arr.size(); i++){
+            auto it = hash.find(arr[i]);
+            if (it != hash.end()) {
+                return true;
+            }
+        }
+        return false;  
+        
+         
+    }
 };
+
+static bool     _foo = ios::sync_with_stdio(false);
+static ostream* _bar = cin.tie(NULL);*/
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < arr.size(); i++)
+        {
+            if(mp.find(arr[i] * 2) != mp.end())
+                return true;
+            if(arr[i] % 2 == 0 && mp.find(arr[i]/2) != mp.end())
+                return true;
+            mp[arr[i]]++;
+        }
+        return false;
+    }
+};
+
+static bool     _foo = ios::sync_with_stdio(false);
+static ostream* _bar = cin.tie(NULL);
